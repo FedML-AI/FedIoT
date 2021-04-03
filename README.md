380f558365a8900a42bfc36f49a5cad9123a74b5
chaoyanghe <me@chaoyanghe.com>
2021-04-02 19:11:10 -0700

    refactoring

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# FedML-IoT-V
+# FedIoT: Federated Learning for Internet of Things
+ 
+<!-- This is FedIoT, an application ecosystem for federated IoT based on FedML framework (https://github.com/FedML-AI/FedML). -->
+
+FedIoT is a research-oriented benchmarking framework for advancing *federated learning* (FL) in *Internet of Things* (IoTs).
+It uses FedML repository as the git submodule. In other words, FedIoT only focuses on advanced models and dataset, while FedML supports various
+federated optimizers (e.g., FedAvg) and platforms (Distributed Computing, IoT/Mobile, Standalone).
+
+## Installation
+<!-- http://doc.fedml.ai/#/installation -->
+After `git clone`-ing this repository, please run the following command to install our dependencies.
+
+```bash
+conda create -n fediot python=3.7
+conda activate fediot
+# conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch -n fediot
+pip install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+pip install -r requirements.txt 
+cd FedML; git submodule init; git submodule update; cd ../;
+```
+
+
+
+## Data Preparation
+
+### Option 1: download our processed files from Amazon S3.
+Download files for each dataset using this command. For example, we could download UCI-MLR by the following command:
+```bash
+cd data/UCI-MLR/
+sh download.sh
+cd ../..
+```
+
+## Experiments for Centralized Learning 
+
+## Experiments for Federated Learning
+
+## Update FedML Submodule
+
+<!-- ### Update FedML Submodule 
+This is only for internal contributors, can put this kind of info to a seperate readme file.
+```
+cd FedML
+git checkout master && git pull
+cd ..
+git add FedML
+git commit -m "updating submodule FedML to latest"
+git push
+```  -->
+
+## Code Structure of FedIoT
+<!-- Note: The code of FedIoT only uses `FedML/fedml_core` and `FedML/fedml_api`.
+In near future, once FedML is stable, we will release it as a python package. 
+At that time, we can install FedML package with pip or conda, without the need to use Git submodule. -->
+
+- `FedML`: a soft repository link generated using `git submodule add https://github.com/FedML-AI/FedML`.
+
+
+- `data`: provide data downloading scripts and store the downloaded datasets.
+Note that in `FedML/data`, there also exists datasets for research, but these datasets are used for evaluating federated optimizers (e.g., FedAvg) and platforms.
+FedNLP supports more advanced datasets and models.
+
+- `data_preprocessing`: data loaders, partition methods and utility functions
+
+- `model`: IoT models. For example, VAE for outlier detection.
+
+- `training`: please define your own `trainer.py` by inheriting the base class in `FedML/fedml-core/trainer/fedavg_trainer.py`.
+Some tasks can share the same trainer.
+
+- `experiments/distributed`: 
+1. `experiments` is the entry point for training. It contains experiments in different platforms. We start from `distributed`.
+2. Every experiment integrates FOUR building blocks `FedML` (federated optimizers), `data_preprocessing`, `model`, `trainer`.
+3. To develop new experiments, please refer the code at `experiments/distributed/text-classification`.
+
+- `experiments/centralized`: 
+1. please provide centralized training script in this directory. 
+2. This is used to get the reference model accuracy for FL. 
+3. You may need to accelerate your training through distributed training on multi-GPUs and multi-machines. Please refer the code at `experiments/centralized/DDP_demo`.
+
+
+
+
+## Citation
+Please cite our FedNLP and FedML paper if it helps your research.
+You can describe us in your paper like this: "We develop our experiments based on FedNLP [1] and FedML [2]".
+
+ 
---
+++ experiments/centralized/main_uci.py
@@ -0,0 +1,180 @@
+import argparse
+import logging
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import wandb
+from matplotlib import pyplot as plt
+
+from model.vae import VAE
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../")))
+
+from model.ae import AutoEncoder
+
+
+def add_args(parser):
+    """
+    parser : argparse.ArgumentParser
+    return a parser added with args required by fit
+    """
+    # Training settings
+    parser.add_argument('--model', type=str, default='vae', metavar='N',
+                        help='neural network used in training')
+
+    parser.add_argument('--dataset', type=str, default='UCI_MLR', metavar='N',
+                        help='dataset used for training')
+
+    parser.add_argument('--data_dir', type=str, default='./../../data/UCI-MLR',
+                        help='data directory')
+    # model related
+    parser.add_argument('--model', type=str, default='vae',
+                        help='model (default: vae): ae, vae')
+
+    # optimizer related
+    parser.add_argument('--batch_size', type=int, default=32, metavar='N',
+                        help='input batch size for training (default: 64)')
+
+    parser.add_argument('--client_optimizer', type=str, default='adam',
+                        help='SGD with momentum; adam')
+
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
+
+    parser.add_argument('--wd', help='weight decay parameter;', type=float, default=0.001)
+
+    parser.add_argument('--epochs', type=int, default=50, metavar='EP',
+                        help='how many epochs will be trained')
+
+    args = parser.parse_args()
+    return args
+
+
+def load_data(args):
+    path_benin_traffic = args.data_dir + '/benign_traffic.csv'
+    path_ack_traffic = args.data_dir + '/ack.csv'
+    logging.info(path_benin_traffic)
+    logging.info(path_ack_traffic)
+
+    db_benigh = pd.read_csv(path_benin_traffic)
+    db_attack = pd.read_csv(path_ack_traffic)
+    # trainset = torch.Tensor(db_benigh)
+    # testset = torch.Tensor(db_attack[0:round(len(db_attack) * 0.2)])
+    # trainset = (trainset - trainset.mean()) / (trainset.std())
+    # testset = (testset - testset.mean()) / (testset.std())
+    # trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+    # testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=0)
+    # return trainloader, testloader, len(trainset), len(testset)
+    trainset = db_benigh[0:round(len(db_benigh) * 0.8)]
+    test_benigh = db_benigh[round(len(db_benigh) * 0.9):len(db_benigh)]
+    test_attack = db_attack[round(len(db_attack) * 0.1):round(len(db_attack) * 0.2)]
+    testset = pd.concat([test_benigh, test_attack])
+    trainset = (trainset - trainset.mean()) / (trainset.std())
+    testset = (testset - testset.mean()) / (testset.std())
+    trainset = np.array(trainset)
+    testset = np.array(testset)
+    testset = torch.Tensor(testset)
+    trainset = torch.Tensor(trainset)
+    testratio = 1 - abs((round(len(db_benigh) * 0.9) - len(db_benigh)) / len(testset))
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=0)
+    return trainloader, testloader, len(trainset), len(testset), testratio
+
+
+def create_model(args):
+    if args.model == "vae":
+        model = VAE()
+    elif args.model == "ae":
+        model = AutoEncoder()
+    else:
+        raise Exception("no such model")
+    logging.info(model)
+    return model
+
+def train(args, model, trainloader):
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    loss_func = nn.MSELoss()
+    for epoch in range(args.epochs):
+
+        # mini- batch loop
+        epoch_loss = 0.0
+        for idx, inp in enumerate(trainloader):
+            optimizer.zero_grad()
+            decode = model(inp)
+            loss = loss_func(decode, inp)
+            epoch_loss += loss.item() / args.batch_size
+            loss.backward()
+            optimizer.step()
+        logging.info("epoch = %d, epoch_loss = %f" % (epoch, epoch_loss))
+        wandb.log({"loss": epoch_loss, "epoch": epoch})
+    logging.info("batch size = %d" % args.batch_size)
+
+    i = []
+    model.eval()
+    for idx, inp in enumerate(trainloader):
+        i.append(torch.sum(abs(model(inp) - inp)))
+    i = torch.tensor(i)
+    test = np.array(i)
+    plt.hist(test, bins='auto', density=True)
+    plt.show()
+    threshold = (torch.mean(i) + 1 * torch.std(i)) / args.batch_size
+    test = np.array(i)
+    plt.hist(test, bins='auto', density=True)
+    plt.show()
+    wandb.log({"threshold": threshold})
+    return threshold
+
+
+def test(args, model, testloader, test_len, testratio):
+    model.eval()
+    anmoaly = []
+    for idx, inp in enumerate(testloader):
+        decode = model(inp)
+        diff = torch.sum(abs(inp - decode))
+        if diff > threshold:
+            anmoaly.append(idx)
+    an_ratio = (len(anmoaly) / test_len)
+    precision = 1 - (abs(an_ratio - testratio) / testratio)
+    print('The accuracy is ', precision)
+    len(trainloader.dataset)
+
+    torch.save(model.state_dict(), "vae_v1.pth")
+    print("Saved PyTorch Model State to model.pth")
+    wandb.log({"Precision": precision})
+    return precision
+
+
+if __name__ == "__main__":
+    # logging.basicConfig(level=logging.INFO,
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S')
+
+    # parse python script input parameters
+    parser = argparse.ArgumentParser()
+    args = add_args(parser)
+    logging.info(args)
+
+    # experimental result tracking
+    wandb.init(project='fediot', entity='automl', config=args)
+
+    # PyTorch configuration
+    torch.set_default_tensor_type(torch.DoubleTensor)
+
+    # load data
+    trainloader, testloader, train_len, test_len, test_ratio = load_data(args)
+
+    # create model
+    model = create_model(args)
+
+    # start training
+    threshold = train(args, model, trainloader)
+    logging.info("threshold = %f" % threshold)
+
+    # start test
+    precision = test(args, testloader, test_len, test_ratio)
