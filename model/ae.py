e5857c0a6a343e10c6c06f33fddfbb436ae8d670
Tuo Zhang <tuozhang@usc.edu>
2021-04-08 20:11:42 +0800

    Update ae.py
    new autoencoder

--- model/ae.py
+++ model/ae.py
@@ -5,26 +5,31 @@ class AutoEncoder(nn.Module):
     def __init__(self):
         super(AutoEncoder, self).__init__()
         self.enc = nn.Sequential(
-            nn.Linear(115, 64),
+            nn.Linear(115, round(115*0.75)),
             nn.ReLU(),
-            nn.Dropout(p=0.1),
-            nn.Linear(64, 32),
+            nn.Dropout(p = 0.1),
+            nn.Linear(round(115*0.75), round(115*0.50)),
             nn.ReLU(),
-            nn.Dropout(p=0.1),
-            nn.Linear(32, 16),
+            nn.Dropout(p = 0.1),
+            nn.Linear(round(115*0.50), round(115*0.33)),
+            nn.ReLU(),
+            nn.Dropout(p = 0.1),
+            nn.Linear(round(115*0.33), round(115*0.25)),
             nn.ReLU()
         )
         self.dec = nn.Sequential(
-            nn.Linear(16, 32),
+            nn.Linear(round(115*0.25), round(115*0.33)),
             nn.ReLU(),
-            nn.Dropout(p=0.1),
-            nn.Linear(32, 64),
+            nn.Dropout(p = 0.1),
+            nn.Linear(round(115*0.33), round(115*0.50)),
             nn.ReLU(),
-            nn.Dropout(p=0.1),
-            nn.Linear(64, 115),
-            nn.ReLU()
+            nn.Dropout(p = 0.1),
+            nn.Linear(round(115*0.50), round(115*0.75)),
+            nn.ReLU(),
+            nn.Dropout(p = 0.1),
+            nn.Linear(round(115*0.75), 115),
+            nn.ReLU(),
         )
-
     def forward(self, x):
         encode = self.enc(x)
         decode = self.dec(encode)
