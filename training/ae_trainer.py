fe9f2b8bbc06e9e7b55aa1f05ca25693d089640e
Tuo Zhang <tuozhang@usc.edu>
2021-04-11 02:25:07 +0800

    will a new arg influence?

--- training/ae_trainer.py
+++ training/ae_trainer.py
@@ -2,6 +2,7 @@ import logging
 
 import numpy as np
 import torch
+import torch.nn as nn
 import wandb
 
 from FedML.fedml_core.trainer.model_trainer import ModelTrainer
@@ -37,23 +38,27 @@ class AETrainer(ModelTrainer):
 
     def test_local(self, client_index, b_global, threshold, test_data, device, args):
         model = self.model.to(device)
+        self.model.eval()
         anmoaly = []
+        thres_func = nn.MSELoss()
         for idx, inp in enumerate(test_data):
             inp = inp.to(device)
             decode = model(inp)
-            diff = torch.sum(abs(inp - decode))
+            diff = thres_func(decode, inp)
             if diff > threshold:
                 anmoaly.append(idx)
         # batch size = 1 so we can use the batch number as the length
-        test_len = len(test_data)
-        precision = (len(anmoaly) / test_len)
-        logging.info('(global = %s) client_index = %d, the local precision is %f' % (str(b_global), client_index, precision))
-        return precision
+        #test_len = len(test_data)
+        anmoaly_len = len(anmoaly)
+        logging.info('(global = %s) client_index = %d, the local detected anmoaly number is %f' % (str(b_global), client_index, anmoaly_len))
+        return anmoaly_len
 
+    #how will adding a new parameter in the func influence the structure?
     def test_on_the_server(self, train_data_local_dict, test_data_local_dict, device, args=None) -> bool:
         logging.info(device)
         threshold_dict = dict()
         diff_results_global = []
+        thres_func = nn.MSELoss()
         for client_index in train_data_local_dict.keys():
             train_data = train_data_local_dict[client_index]
             diff_results_per_client = []
@@ -61,7 +66,7 @@ class AETrainer(ModelTrainer):
             for idx, inp in enumerate(train_data):
                 inp = inp.to(device)
                 decode = self.model(inp)
-                diff = torch.sum(abs(decode - inp))
+                diff = thres_func(decode, inp)
                 diff_results_per_client.append(diff)
                 diff_results_global.append(diff)
             diff_results_per_client = torch.tensor(diff_results_per_client)
