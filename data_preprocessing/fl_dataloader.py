4a4c99292b14ad2e6a1df6c1433fb7e303bd5372
Tuo Zhang <tuozhang@usc.edu>
2021-04-11 02:24:28 +0800

    add opt set into account

--- data_preprocessing/fl_dataloader.py
+++ data_preprocessing/fl_dataloader.py
@@ -50,24 +50,34 @@ def add_args(parser):
 
 
 # read the data from the csv
-def load_data(args, train_file_name, test_file_name):
-    path_data_train = args.data_dir + "/" + train_file_name
-    path_data_test = args.data_dir + "/" + test_file_name
-    logging.info(path_data_train)
-    logging.info(path_data_test)
-    db_train = pd.read_csv(path_data_train)
-    db_test = pd.read_csv(path_data_test)
-    db_train = (db_train - db_train.mean()) / (db_train.std())
-    db_test = (db_test - db_test.mean()) / (db_test.std())
-    db_train = np.array(db_train)
-    db_test = np.array(db_test)
-    db_test[np.isnan(db_test)] = 0
-    trainset = db_train
-    testset = db_test
+def load_data(args, benign_file_name, attack_file_name):
+    path_benin_traffic = args.data_dir + "/" + benign_file_name
+    path_ack_traffic = args.data_dir + "/" + attack_file_name
+    logging.info(path_benin_traffic)
+    logging.info(path_ack_traffic)
+
+
+    db_benign = pd.read_csv(path_benin_traffic)
+    db_attack = pd.read_csv(path_ack_traffic)
+    db_benign = (db_benign - db_benign.mean()) / (db_benign.std())
+    db_attack = (db_attack - db_attack.mean()) / (db_attack.std())
+    trainset = db_benign[0:round(len(db_benign) * 0.67)]
+    optset = db_benign[round(len(db_benign) * 0.67):len(db_benign)]
+    testset = pd.concat([db_benign[round(len(db_benign) * 0.67):len(db_benign)], db_attack])
+    trainset = np.array(trainset)
+    optset = np.array(optset)
+    testset = np.array(testset)
+    trainset[np.isnan(trainset)] = 0
+    optset[np.isnan(optset)] = 0
+    trainset[np.isnan(trainset)] = 0
+
     len_train = len(trainset)
     len_test = len(testset)
-    correct_ratio = 1 - (0.5*len_train)/len_test
-    return len_train, len_test, trainset, testset, correct_ratio
+    correct_ratio = 1 - len(trainset) / (2 * len(testset))
+    # trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+    # optloader = torch.utils.data.DataLoader(optset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+    # testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=0)
+    return len_train, len_test, trainset, testset, optset, correct_ratio
 
 
 def homo_partition_data(args, process_id, dataset):
@@ -82,13 +92,16 @@ def homo_partition_data(args, process_id, dataset):
     return net_dataidx_map, train_data_local_num_dict
 
 
-def local_dataloader(args, train_file_name, test_file_name, process_id):
+def local_dataloader(args, benign_file_name, attack_file_name, process_id):
     train_data_local_dict = dict()
     test_data_local_dict = dict()
-    train_data_num, test_data_num, train_data_global, test_data_global, correct_ratio = load_data(args, train_file_name, test_file_name)
+    opt_data_local_dict = dict()
+    train_data_num, test_data_num, train_data_global, test_data_global, opt_data_global, correct_ratio \
+        = load_data(args, benign_file_name, attack_file_name)
 
     dataidx_map_train, train_data_local_num_dict = homo_partition_data(args, process_id, train_data_global)
     dataidx_map_test, test_data_local_num_dict = homo_partition_data(args, process_id, test_data_global)
+    dataidx_map_opt, opt_data_local_num_dict = homo_partition_data(args, process_id, opt_data_global)
 
     # for local train data
     for client_idx in range(args.client_num_in_total):
@@ -106,8 +119,15 @@ def local_dataloader(args, train_file_name, test_file_name, process_id):
                                                                         num_workers=0)
         #logging.info("client_idx = %d, local_test_sample_number = %d" % (client_idx, test_data_local_num_dict[client_idx]))
 
-    return train_data_num, test_data_num, train_data_global, test_data_global, \
-           train_data_local_num_dict, train_data_local_dict, test_data_local_dict, correct_ratio
+    # for local opt data
+    for client_idx in range(args.client_num_in_total):
+        data_local_opt = opt_data_global[dataidx_map_opt[client_idx]]
+        opt_data_local_dict[client_idx] = torch.utils.data.DataLoader(data_local_opt, batch_size=args.batch_size,
+                                                                        shuffle=False,
+                                                                        num_workers=0)
+
+    return train_data_num, test_data_num, train_data_global, test_data_global, opt_data_global, \
+           train_data_local_num_dict, train_data_local_dict, test_data_local_dict, opt_data_local_dict, correct_ratio
 
 
 if __name__ == "__main__":
@@ -121,8 +141,8 @@ if __name__ == "__main__":
     args = add_args(parser)
     logging.info(args)
 
-    dataset = local_dataloader(args,'/Danmini_Doorbell/doorbell_train_norm.csv', '/Danmini_Doorbell/doorbell_test_norm.csv', 1)
-    [train_data_num, test_data_num, train_data_global, test_data_global,
-     train_data_local_num_dict, train_data_local_dict, test_data_local_dict, correct_ratio] = dataset
+    dataset = local_dataloader(args,'/Danmini_Doorbell/Danmini_Doorbell_benign_raw.csv', '/Danmini_Doorbell/Danmini_Doorbell_atk_raw.csv', 1)
+    [train_data_num, test_data_num, train_data_global, test_data_global, opt_data_global,
+     train_data_local_num_dict, train_data_local_dict, test_data_local_dict, opt_data_local_dict, correct_ratio] = dataset
 
 
