37cb66a608418c6955eec870ce7ed701aa01f86e
Tuo Zhang <tuozhang@usc.edu>
2021-04-13 15:43:24 +0800

    Update ae.py
    delete wrong activation function layer

--- model/ae.py
+++ model/ae.py
@@ -15,7 +15,7 @@ class AutoEncoder(nn.Module):
             nn.Tanh(),
             #nn.Dropout(p = 0.1),
             nn.Linear(round(115*0.33), round(115*0.25)),
-            nn.Tanh()
+
         )
         self.dec = nn.Sequential(
             nn.Linear(round(115*0.25), round(115*0.33)),
@@ -28,7 +28,7 @@ class AutoEncoder(nn.Module):
             nn.Tanh(),
             #nn.Dropout(p = 0.1),
             nn.Linear(round(115*0.75), 115),
-            nn.Tanh()
+
         )
     def forward(self, x):
         encode = self.enc(x)
