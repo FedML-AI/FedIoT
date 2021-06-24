cbbd8ecdab82616397b44fba20c88b8218fd3318
Tuo Zhang <tuozhang@usc.edu>
2021-06-24 15:25:01 -0700

    Update ae_trainer.py

--- training/ae_trainer.py
+++ training/ae_trainer.py
@@ -38,11 +38,13 @@ class AETrainer(ModelTrainer):
                     # epoch_loss += loss.item() / args.batch_size
                     loss.backward()
                     optimizer.step()
+            logging.info('Epoch training complete')
 
     def test(self, test_data, device, args):
         pass
 
-    def test_local(self, client_index, opt_idx_threshold, threshold, test_data, device, args):
+    def test_local(self, client_index, threshold, train_data, test_data, device, args):
+        # pass
         model = self.model.to(device)
         self.model.eval()
 
@@ -52,22 +54,24 @@ class AETrainer(ModelTrainer):
         false_negative = []
         thres_func = nn.MSELoss()
 
-        for idx, inp in enumerate(test_data):
-            inp = inp.to(device)
-            decode = model(inp)
-            diff = thres_func(decode, inp)
-            mse = diff.item()
-            # print("IDX is ", idx, "MSE is ", mse)
-            if idx < opt_idx_threshold:
+        for idx, inp in enumerate(train_data):
+            if idx >= round(len(train_data) - 5000/args.batch_size):
+                inp = inp.to(device)
+                diff = thres_func(model(inp), inp)
+                mse = diff.item()
                 if mse > threshold:
                     false_positive.append(idx)
                 else:
                     true_negative.append(idx)
+
+        for idx, inp in enumerate(test_data):
+            inp = inp.to(device)
+            diff = thres_func(model(inp), inp)
+            mse = diff.item()
+            if mse > threshold:
+                true_positive.append(idx)
             else:
-                if mse > threshold:
-                    true_positive.append(idx)
-                else:
-                    false_negative.append(idx)
+                false_negative.append(idx)
 
         # print(len(true_positive))
         # print(len(false_positive))
@@ -93,43 +97,54 @@ class AETrainer(ModelTrainer):
         mse_results_global = []
         threshold_dict = {}
         thres_func = nn.MSELoss()
-
-        opt_threshold = [round(4955.0 * 0.67 / args.batch_size), round(1311.0 * 0.67 / args.batch_size),
-                         round(3910.0 * 0.67 / args.batch_size), round(17524.0 * 0.67 / args.batch_size),
-                         round(6215.0 * 0.67 / args.batch_size), round(9851.0 * 0.67 / args.batch_size),
-                         round(5215.0 * 0.67 / args.batch_size), round(4658.0 * 0.67 / args.batch_size),
-                         round(1953.0 * 0.67 / args.batch_size)]
-
-        test_threshold = 1000
+        #
+        # opt_threshold = [round(4955.0 * 0.67 / args.batch_size), round(1311.0 * 0.67 / args.batch_size),
+        #                  round(3910.0 * 0.67 / args.batch_size), round(17524.0 * 0.67 / args.batch_size),
+        #                  round(6215.0 * 0.67 / args.batch_size), round(9851.0 * 0.67 / args.batch_size),
+        #                  round(5215.0 * 0.67 / args.batch_size), round(4658.0 * 0.67 / args.batch_size),
+        #                  round(1953.0 * 0.67 / args.batch_size)]
+        #
+        # test_threshold = 1000
 
         for client_index in train_data_local_dict.keys():
             opt_data = train_data_local_dict[client_index]
             mse_results_per_client = []
             self.model.eval()
             for idx, inp in enumerate(opt_data):
-                if idx >= opt_threshold[client_index]:
+                if idx >= round(len(opt_data) * 2 / 3):
                     inp = inp.to(device)
                     decode = self.model(inp)
                     diff = thres_func(decode, inp)
                     mse = diff.item()
                     mse_results_per_client.append(mse)
-                    # mse_results_global.append(mse)
+                    mse_results_global.append(mse)
             mse_results_per_client = torch.tensor(mse_results_per_client)
             threshold_dict[client_index] = torch.mean(mse_results_per_client) + 1 * torch.std(mse_results_per_client) / np.sqrt(
             args.batch_size)
-        threshold_path = os.path.join("/Users/ultraz/PycharmProjects/FedML-IoT-V/experiments/distributed", 'threshold_dict.pkl')
-        joblib.dump(threshold_dict, threshold_path)
+
+        # threshold_path = os.path.join("/Users/ultraz/PycharmProjects/FedML-IoT-V/experiments/distributed", 'threshold_dict.pkl')
+        # joblib.dump(threshold_dict, threshold_path)
 
 
-        # mse_results_global = torch.tensor(mse_results_global)
-        # threshold_global =torch.mean(mse_results_global) + 1 * torch.std(mse_results_global)/ np.sqrt(args.batch_size)
-        # logging.info('The threshold is %f' % (threshold_global))
+        mse_results_global = torch.tensor(mse_results_global)
+        threshold_global =torch.mean(mse_results_global) + 1 * torch.std(mse_results_global)/ np.sqrt(args.batch_size)
+        logging.info('The threshold is %f' % (threshold_global))
 
         accuracy_array_global = []
         precision_array_global = []
         fpr_array_global = []
 
-        model_save_dir = "/Users/ultraz/PycharmProjects/FedML-IoT-V/experiments/distributed"
+        for client_index in test_data_local_dict.keys():
+            test_data = test_data_local_dict[client_index]
+            # using global threshold for test
+            # [accuracy_client, precision_client, fpr_client] = self.test_local(client_index,
+            #                                                                   (test_threshold[client_index] / 2), threshold_global, test_data, device, args)
+            [accuracy_client, precision_client, fpr_client] = self.test_local(client_index, threshold_global, train_data_local_dict[client_index], test_data, device, args)
+            accuracy_array_global.append(accuracy_client)
+            precision_array_global.append(precision_client)
+            fpr_array_global.append(fpr_client)
+
+        model_save_dir = "/Users/ultraz/PycharmProjects/FedDetect/training"
         path = os.path.join(model_save_dir, 'model.ckpt')
         torch.save(self.model.state_dict(), path)
 
